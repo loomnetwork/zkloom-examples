60bfcd923524549480b53cb0be2ae2d20842b1db
Vadim Macagon <vadim.macagon@gmail.com>
2024-03-28 00:08:49 +0700

    Update the testnet deployment example

--- 01-testnet-deploy/hardhat.config.ts
+++ 01-testnet-deploy/hardhat.config.ts
@@ -3,21 +3,16 @@ import "@matterlabs/hardhat-zksync-deploy";
 import "@matterlabs/hardhat-zksync-solc";
 
 const config: HardhatUserConfig = {
-  defaultNetwork: "zkLoomTestnet",
+  defaultNetwork: "zkLoomSepolia",
   networks: {
-    zkLoomTestnet: {
-      url: "http://zkloom-t1.dappchains.com:2052",
+    zkLoomSepolia: {
+      url: "http://zkloom-sepolia.dappchains.com:2052",
       zksync: true,
-      ethNetwork: "http://zkloom-t1.dappchains.com:2082"
+      ethNetwork: "sepolia"
     },
   },
   solidity: {
-    version: "0.8.16",
-  },
-  zksolc: {
-    version: "1.3.10",
-    compilerSource: "binary",
-    settings: {},
+    version: "0.8.17",
   }
 };
 
